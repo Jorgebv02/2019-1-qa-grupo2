2671593cfbb3118cf112eda287e8052f802e0f9a
José Andrés Ceciliano Granados <cecilianogranados96@gmail.com>
2019-04-21 10:58:38 -0600

    [Nuevo][Modificado]
    - Framework de pruebas final
    - Modificaciones de especificaciones
    - Modificaciones de pruebas
    - Nuevas pruebas añadidas

---
+++ application/tests/models/Login_test.php
@@ -0,0 +1,42 @@
+<?php
+class Login_test extends TestCase
+{
+    public function setUp(){
+		//$this->obj = $this->newModel('login_m');
+	}
+    
+    public function test_index(){
+        $output = $this->request('GET', '');
+        $this->assertContains('<h1 class="white-text"><b>FaceTec</b></h1>', $output);
+    }
+    
+    public function test_index_404(){
+        $this->request('GET', ['nocontroller', 'noaction']);
+		$this->assertResponseCode(404);
+    }
+    
+	public function test_login_ingresar_sin_parametros(){
+        $output = $this->request('GET',['login', 'ingresar']);
+        $this->assertRedirect('../../index.php?error_login=1', 302);
+    }
+
+    public function test_login_ingresar_usuario_correcto_pass_incorrecto(){
+        $output = $this->request(
+			'POST',
+			['login', 'ingresar'],
+			['usuario' => 'ceciliano','password' => 'admin2']
+		);
+        $this->assertRedirect('../../index.php?error_login=2', 302);
+    }
+    
+    public function test_login_ingresar_usuario_correcto_pass_correcto(){
+        $output = $this->request(
+			'POST',
+			['login', 'ingresar'],
+			['usuario' => 'ceciliano','password' => 'admin']
+		);
+        $this->assertRedirect('../index.php/muro', 302);
+    } 
+    
+    
+}
